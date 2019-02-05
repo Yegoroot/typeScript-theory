ea9ede1d3c1868d7b1daf1ee01e29499ea168549
e.kirsanov <e.kirsanov@netris.ru>
2019-02-05 19:41:42 +0300

    +

---
+++ 1-function.ts
@@ -0,0 +1,13 @@
+// ОБЫЧНАЯ
+function add(a, b) {
+	return a + b
+}
+
+// ТИПИЗИРОВАННАЯ
+/**
+ * в этой ф-ии мы ждем числа !important
+ * и возвращаем число !important
+ */
+function addType(x: number, y: number): number {
+	return x + y
+}

3adbf2d36b097ae29925cb6fe131c086e3a03592
e.kirsanov <e.kirsanov@netris.ru>
2019-02-06 12:29:30 +0300

    модификаторы доступы в класс

--- class.ts
+++ class.ts
@@ -1,13 +1,19 @@
 class Message {
-    messageText: string;
+	// модификаторы доступа
+	public messageText: string // доступный метод для всех
+	private priv: number // доступный метод внутри класса
+	protected prot: string // доступен для класса и ближайших потомков
 
-    constructor(text) {
-        this.messageText = text;
-    }
-    showAlert() {
-        alert(this.messageText)
-    }
+	constructor(text: string, priv: number, prot: string) {
+		this.messageText = text
+	}
+	// constructor(public text: string, private priv: number) {
+	// 	this.messageText = text
+	// }
+	method() {
+		alert(this.messageText)
+	}
 }
 
-let text = new Message ('hello')
-text.showAlert()
+let text = new Message('hello', 123, 'asd')
+text.method()

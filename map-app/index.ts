1bd75c4c82b847860639931b5eca6279fd7f2e83
Amiroot <easywayroot@gmail.com>
2021-01-10 20:51:05 +0300

    map Class

--- map-app/index.ts
+++ map-app/index.ts
@@ -1,9 +1,6 @@
 import {User} from './User'
 import {Company} from './Company'
+import {CustomMap} from './CustomMap'
 
+new CustomMap('map')
 
-
-const user = new User()
-const company = new Company()
-
-console.log(user, company)

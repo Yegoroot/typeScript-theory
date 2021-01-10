746b58e7c514dfad8174da1cead8fef0c7723d1c
Amiroot <easywayroot@gmail.com>
2021-01-10 22:41:35 +0300

    customMap class

--- map-app/CustomMap.ts
+++ map-app/CustomMap.ts
@@ -1,3 +1,18 @@
+import {User} from './User'
+import {Company} from './Company'
+
+/**
+ * Instructions to every other class
+ * on how they can be an argument to 'addMarker'
+ */
+
+interface Mappable {
+  location: {
+    lat: number
+    lng: number
+  }
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map
 
@@ -11,6 +26,15 @@ export class CustomMap {
     })
   }
 
+  addMarker(mappable: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      }
+    })
+  }
 
 }
 

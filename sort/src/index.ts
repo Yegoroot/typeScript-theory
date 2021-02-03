14440b2516fb3fa0c39ec1c0d7cff9727b8c6368
Amiroot <easywayroot@gmail.com>
2021-02-03 18:11:26 +0300

    characters collections

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,8 +1,16 @@
 import {Sorter} from './Sorter'
-import {NumbersCollection} from './NumbersCollection'
+import {CharactersCollection} from './CharactersCollection'
+// import {NumbersCollection} from './NumbersCollection'
 
-const numbersCollection = new NumbersCollection([10, 3, -1, -5, 1])
-const sorter = new Sorter(numbersCollection)
+// const numbersCollection = new NumbersCollection([10, 3, -1, -5, 1])
+// const sorter = new Sorter(numbersCollection)
+// sorter.sort()
+// console.log(numbersCollection.data)
+
+
+const charactersCollection = new CharactersCollection('XZWXaaqwq')
+const sorter = new Sorter(charactersCollection)
 
 sorter.sort()
-console.log(numbersCollection.data)
+
+console.log(charactersCollection.data)
